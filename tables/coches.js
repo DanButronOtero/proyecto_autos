a7123c6be70f69090ccad5906644a13ad129a2ea
danbutron <dan.butron@gmail.com>
2021-06-02 01:11:29 -0500

    read y update coches

--- tables/coches.js
+++ tables/coches.js
@@ -4,9 +4,22 @@ const { Router } = require('express');
 const router = Router();
 
 
-router.get('/query', (req, res) => {
-    conn.query('select * from coches', (error, results, fields) => {
-        res.send(results);
+router.get('/coches', (req, res) => {
+    let coches;
+    conn.query('select c.id,c.matricula,c.no_motor,c.id_modelo,c.estado_logico,m.marca,m.modelo,m.anio from coches as c INNER JOIN modelo as m on c.id_modelo = m.id ', (error, results, fields) => {
+        coches = results;
+        res.render('pages/cruds/coches', { registros: coches });
+    });
+
+
+});
+router.post('/coches/delete', (req, res) => {
+    console.log(req.body.id);
+    console.log(Object.keys(req.body));
+    let query = `DELETE FROM coches WHERE id = '${req.body.id}';`;
+    console.log(query);
+    conn.query(query, (error, results, fields) => {
+        res.redirect('/coches');
     });
 });
 
