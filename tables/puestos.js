bf2511719fa410de4535c5a2491e96ccc37a1427
ulises520689 <86495570+ulises520689@users.noreply.github.com>
2021-06-25 22:14:40 -0500

    Add files via upload

--- tables/puestos.js
+++ tables/puestos.js
@@ -4,38 +4,49 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/puestos', (req, res) => {
-    connection.query('select * from puestos;', (error, results, fields) => {
+
+    conn.query('select * from puestos;', (error, results, fields) => {
+
         res.render('pages/cruds/puestos/puestos', { registros: results });
     });
-});
-// delete from puestos where id=id;
-router.post('/puestos/delete', (req, res) => {
-    connection.query(`delete from puestos where id=${req.body.id};`, (error, results, fields) => {
-        res.redirect('/puestos')
-    });
+
+
 });
 
 router.get('/puestos/create', (req, res) => {
-    res.render('pages/cruds/puestos/create');
+    res.render('pages/cruds/puestos/create.ejs');
 });
 
 router.post('/puestos/create', (req, res) => {
-    let query = `insert into puestos values('','${req.body.nombre}','${req.body.descripcion}');`;
-    connection.query(query, (error, results, fields) => {
+
+    let query = `insert into puestos values("","${req.body.nombre}","${req.body.descripcion}") ;`;
+
+    conn.query(query, (error, results, fields) => {
+
         res.redirect('/puestos');
     });
 });
 
-router.get('/puestos/update', (req, res) => {
-    let query = `select * from puestos where id = ${req.query.id};`;
-    connection.query(query, (error, results, fields) => {
-        res.render('pages/cruds/puestos/update', { puestos: results[0] });
+router.post('/puestos/delete', (req, res) => {
+    let query = `delete from puestos WHERE id = '${req.body.id}';`;
+
+    conn.query(query, (error, results, fields) => {
+        res.redirect('/puestos');
     });
 });
 
+
+router.get('/puestos/update', (req, res) => {
+    let query = `select * from puestos where id ="${req.query.id}";`;
+    conn.query(query, (error, results, fields) => {
+        res.render('pages/cruds/puestos/update.ejs', { puestos: results[0] });
+    });
+
+});
 router.post('/puestos/update', (req, res) => {
-    let query = `update puestos set nombre='${req.body.nombre}',descripcion='${req.body.descripcion}' where id=${req.body.id};`;
-    connection.query(query, (error, results, fields) => {
+    let query = `update puestos set nombre ="${req.body.nombre.trim()}",descripcion="${req.body.descripcion.trim()}" WHERE id = '${req.body.id}';`;
+
+    conn.query(query, (error, results, fields) => {
         res.redirect('/puestos');
     });
 });
